23af795945a26d39ac04491ad33e3854b5f9aa74
TueCN <tue.tn1@gmail.com>
2017-01-09 17:13:42 +0100

    Playing around with the top menu bar.. (WIP)

--- drools-wb-webapp/src/main/java/org/drools/workbench/client/DroolsWorkbenchEntryPoint.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/client/DroolsWorkbenchEntryPoint.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2012 Red Hat, Inc. and/or its affiliates.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,23 +15,14 @@
  */
 package org.drools.workbench.client;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import javax.inject.Inject;
 
 import com.google.gwt.animation.client.Animation;
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
-import org.drools.workbench.client.resources.i18n.AppConstants;
+import org.drools.workbench.client.menu.RecentlyViewedMenuBuilder;
 import org.guvnor.common.services.shared.config.AppConfigService;
 import org.guvnor.common.services.shared.security.KieWorkbenchACL;
 import org.guvnor.common.services.shared.security.KieWorkbenchPolicy;
@@ -40,31 +31,21 @@ import org.jboss.errai.common.client.api.Caller;
 import org.jboss.errai.common.client.api.RemoteCallback;
 import org.jboss.errai.ioc.client.api.AfterInitialization;
 import org.jboss.errai.ioc.client.api.EntryPoint;
-import org.jboss.errai.ioc.client.container.IOCBeanDef;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
-import org.jboss.errai.security.shared.api.Role;
 import org.jboss.errai.security.shared.api.identity.User;
 import org.jboss.errai.security.shared.service.AuthenticationService;
-import org.kie.workbench.common.screens.search.client.menu.SearchMenuBuilder;
 import org.kie.workbench.common.services.shared.preferences.ApplicationPreferences;
 import org.kie.workbench.common.widgets.client.menu.AboutMenuBuilder;
 import org.kie.workbench.common.widgets.client.menu.ResetPerspectivesMenuBuilder;
 import org.uberfire.client.menu.CustomSplashHelp;
-import org.uberfire.client.mvp.AbstractWorkbenchPerspectiveActivity;
+import org.uberfire.client.menu.WorkbenchViewModeSwitcherMenuBuilder;
 import org.uberfire.client.mvp.ActivityManager;
-import org.uberfire.client.mvp.PerspectiveActivity;
 import org.uberfire.client.mvp.PlaceManager;
-import org.uberfire.client.views.pfly.menu.UserMenu;
 import org.uberfire.client.workbench.widgets.menu.UtilityMenuBar;
+import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBar;
 import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBarPresenter;
-import org.uberfire.mvp.Command;
-import org.uberfire.mvp.impl.DefaultPlaceRequest;
 import org.uberfire.workbench.model.menu.MenuFactory;
-import org.uberfire.workbench.model.menu.MenuItem;
-import org.uberfire.workbench.model.menu.MenuPosition;
 import org.uberfire.workbench.model.menu.Menus;
-
-import static org.uberfire.workbench.model.menu.MenuFactory.*;
 
 /**
  * GWT's Entry-point for Drools Workbench
@@ -102,9 +83,6 @@ public class DroolsWorkbenchEntryPoint {
     @Inject
     private UtilityMenuBar utilityMenuBar;
 
-    @Inject
-    private UserMenu userMenu;
-
     @AfterInitialization
     public void startApp() {
         kieSecurityService.call( new RemoteCallback<String>() {
@@ -128,107 +106,15 @@ public class DroolsWorkbenchEntryPoint {
     }
 
     private void setupMenu() {
-        final AbstractWorkbenchPerspectiveActivity defaultPerspective = getDefaultPerspectiveActivity();
-
-        for ( Menus roleMenus : getRoles() ) {
-            userMenu.addMenus( roleMenus );
-        }
 
         final Menus utilityMenus = MenuFactory
-                .newTopLevelCustomMenu( iocManager.lookupBean( CustomSplashHelp.class ).getInstance() )
-                .endMenu()
-                .newTopLevelCustomMenu( iocManager.lookupBean( AboutMenuBuilder.class ).getInstance() )
+                .newTopLevelCustomMenu( iocManager.lookupBean( RecentlyViewedMenuBuilder.class ).getInstance() )
                 .endMenu()
                 .newTopLevelCustomMenu( iocManager.lookupBean( ResetPerspectivesMenuBuilder.class ).getInstance() )
-                .endMenu()
-                .newTopLevelCustomMenu( userMenu )
                 .endMenu()
                 .build();
 
         utilityMenuBar.addMenus( utilityMenus );
-
-        final Menus menus = MenuFactory
-                .newTopLevelMenu( AppConstants.INSTANCE.Home() )
-                .respondsWith( new Command() {
-                    @Override
-                    public void execute() {
-                        if ( defaultPerspective != null ) {
-                            placeManager.goTo( new DefaultPlaceRequest( defaultPerspective.getIdentifier() ) );
-                        } else {
-                            Window.alert( "Default perspective not found." );
-                        }
-                    }
-                } )
-                .endMenu()
-                .newTopLevelMenu( AppConstants.INSTANCE.Perspectives() )
-                .withItems( getPerspectives() )
-                .endMenu()
-                .newTopLevelCustomMenu( iocManager.lookupBean( SearchMenuBuilder.class ).getInstance() )
-                .endMenu()
-                .build();
-
-        menubar.addMenus( menus );
-    }
-
-    private List<Menus> getRoles() {
-        final List<Menus> result = new ArrayList<Menus>( identity.getRoles().size() );
-        result.add( MenuFactory.newSimpleItem( AppConstants.INSTANCE.Logout() ).respondsWith( new LogoutCommand() ).endMenu().build() );
-        for ( final Role role : identity.getRoles() ) {
-            if ( !role.getName().equals( "IS_REMEMBER_ME" ) ) {
-                result.add( MenuFactory.newSimpleItem( AppConstants.INSTANCE.Role() + ": " + role.getName() ).endMenu().build() );
-            }
-        }
-        return result;
-    }
-
-    private List<MenuItem> getPerspectives() {
-        final List<MenuItem> perspectives = new ArrayList<MenuItem>();
-        for ( final PerspectiveActivity perspective : getPerspectiveActivities() ) {
-            final String name = perspective.getDefaultPerspectiveLayout().getName();
-            final MenuItem item = newSimpleItem( name ).perspective( perspective.getIdentifier() ).endMenu().build().getItems().get( 0 );
-            perspectives.add( item );
-        }
-
-        return perspectives;
-    }
-
-    private AbstractWorkbenchPerspectiveActivity getDefaultPerspectiveActivity() {
-        AbstractWorkbenchPerspectiveActivity defaultPerspective = null;
-        final Collection<IOCBeanDef<AbstractWorkbenchPerspectiveActivity>> perspectives = iocManager.lookupBeans( AbstractWorkbenchPerspectiveActivity.class );
-        final Iterator<IOCBeanDef<AbstractWorkbenchPerspectiveActivity>> perspectivesIterator = perspectives.iterator();
-        outer_loop:
-        while ( perspectivesIterator.hasNext() ) {
-            final IOCBeanDef<AbstractWorkbenchPerspectiveActivity> perspective = perspectivesIterator.next();
-            final AbstractWorkbenchPerspectiveActivity instance = perspective.getInstance();
-            if ( instance.isDefault() ) {
-                defaultPerspective = instance;
-                break outer_loop;
-            } else {
-                iocManager.destroyBean( instance );
-            }
-        }
-        return defaultPerspective;
-    }
-
-    private List<PerspectiveActivity> getPerspectiveActivities() {
-
-        //Get Perspective Providers
-        final Set<PerspectiveActivity> activities = activityManager.getActivities( PerspectiveActivity.class );
-
-        //Sort Perspective Providers so they're always in the same sequence!
-        List<PerspectiveActivity> sortedActivities = new ArrayList<PerspectiveActivity>( activities );
-        Collections.sort( sortedActivities,
-                          new Comparator<PerspectiveActivity>() {
-
-                              @Override
-                              public int compare( PerspectiveActivity o1,
-                                                  PerspectiveActivity o2 ) {
-                                  return o1.getDefaultPerspectiveLayout().getName().compareTo( o2.getDefaultPerspectiveLayout().getName() );
-                              }
-
-                          } );
-
-        return sortedActivities;
     }
 
     //Fade out the "Loading application" pop-up
@@ -247,20 +133,6 @@ public class DroolsWorkbenchEntryPoint {
                 e.getStyle().setVisibility( Style.Visibility.HIDDEN );
             }
         }.run( 500 );
-    }
-
-    private class LogoutCommand implements Command {
-
-        @Override
-        public void execute() {
-            authService.call( new RemoteCallback<Void>() {
-                @Override
-                public void callback( final Void response ) {
-                    final String location = GWT.getModuleBaseURL().replaceFirst( "/" + GWT.getModuleName() + "/", "/logout.jsp" );
-                    redirect( location );
-                }
-            } ).logout();
-        }
     }
 
     public static native void redirect( String url )/*-{
