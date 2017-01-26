afeafcdef59bcf155457dd51ad51ed367b3fb272
TueCN <tue.tn1@gmail.com>
2017-01-26 17:32:37 +0100

    Reverted the removal of many menus from WorkbenchMenuBar - we are leaving the full workbench accessible while we are still developing, then we can remove them when we are further along in development

--- drools-wb-webapp/src/main/java/org/drools/workbench/client/DroolsWorkbenchEntryPoint.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/client/DroolsWorkbenchEntryPoint.java
@@ -15,20 +15,24 @@
  */
 package org.drools.workbench.client;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.inject.Inject;
 
 import com.google.gwt.animation.client.Animation;
+import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.Style;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
-import org.drools.workbench.client.menu.IncomingChangesMenuBuilder;
-import org.drools.workbench.client.menu.RecentlyEditedMenuBuilder;
-import org.drools.workbench.client.menu.RecentlyViewedMenuBuilder;
 import org.drools.workbench.client.menu.WorkbenchViewModeSwitcherUtilityMenuBuilder;
+import org.drools.workbench.client.resources.i18n.AppConstants;
 import org.guvnor.common.services.shared.config.AppConfigService;
 import org.guvnor.common.services.shared.security.KieWorkbenchACL;
 import org.guvnor.common.services.shared.security.KieWorkbenchPolicy;
@@ -39,6 +43,8 @@ import org.jboss.errai.ioc.client.api.AfterInitialization;
 import org.jboss.errai.ioc.client.api.EntryPoint;
 import org.jboss.errai.ioc.client.container.IOCBeanDef;
 import org.jboss.errai.ioc.client.container.SyncBeanManager;
+import org.jboss.errai.security.shared.api.Role;
+import org.jboss.errai.security.shared.api.identity.User;
 import org.jboss.errai.security.shared.service.AuthenticationService;
 import org.kie.workbench.common.services.shared.preferences.ApplicationPreferences;
 import org.kie.workbench.common.widgets.client.menu.ResetPerspectivesMenuBuilder;
@@ -48,19 +54,27 @@ import org.uberfire.client.mvp.ActivityManager;
 import org.uberfire.client.mvp.PerspectiveActivity;
 import org.uberfire.client.mvp.PerspectiveManager;
 import org.uberfire.client.mvp.PlaceManager;
+import org.uberfire.client.views.pfly.menu.UserMenu;
 import org.uberfire.client.workbench.widgets.menu.UtilityMenuBar;
 import org.uberfire.client.workbench.widgets.menu.WorkbenchMenuBarPresenter;
 import org.uberfire.mvp.Command;
 import org.uberfire.mvp.PlaceRequest;
+import org.uberfire.mvp.impl.DefaultPlaceRequest;
 import org.uberfire.mvp.impl.ForcedPlaceRequest;
 import org.uberfire.workbench.model.menu.MenuFactory;
+import org.uberfire.workbench.model.menu.MenuItem;
 import org.uberfire.workbench.model.menu.Menus;
+
+import static org.uberfire.workbench.model.menu.MenuFactory.*;
 
 /**
  * GWT's Entry-point for Drools Workbench
  */
 @EntryPoint
 public class DroolsWorkbenchEntryPoint {
+
+    @Inject
+    private User identity;
 
     @Inject
     private Caller<AppConfigService> appConfigService;
@@ -90,6 +104,10 @@ public class DroolsWorkbenchEntryPoint {
     private UtilityMenuBar utilityMenuBar;
 
     @Inject
+    private UserMenu userMenu;
+
+
+    @Inject
     private PerspectiveManager perspectiveManager;
 
     @AfterInitialization
@@ -115,27 +133,38 @@ public class DroolsWorkbenchEntryPoint {
     }
 
     private void setupMenu() {
-//        final AbstractWorkbenchPerspectiveActivity defaultPerspective = getDefaultPerspectiveActivity();
+        final AbstractWorkbenchPerspectiveActivity defaultPerspective = getDefaultPerspectiveActivity();
+
+        for ( Menus roleMenus : getRoles() ) {
+            userMenu.addMenus( roleMenus );
+        }
 
         final Menus utilityMenus = MenuFactory
-                .newTopLevelCustomMenu( iocManager.lookupBean( RecentlyViewedMenuBuilder.class ).getInstance() )
-                .endMenu()
-                .newTopLevelCustomMenu( iocManager.lookupBean( RecentlyEditedMenuBuilder.class ).getInstance() )
-                .endMenu()
-                .newTopLevelCustomMenu( iocManager.lookupBean( IncomingChangesMenuBuilder.class ).getInstance() )
-                .endMenu()
                 .newTopLevelCustomMenu( iocManager.lookupBean( ResetPerspectivesMenuBuilder.class ).getInstance() )
                 .endMenu()
                 .newTopLevelCustomMenu( iocManager.lookupBean( WorkbenchViewModeSwitcherUtilityMenuBuilder.class ).getInstance() )
+                .endMenu()
+                .newTopLevelCustomMenu( userMenu )
                 .endMenu()
                 .build();
 
         utilityMenuBar.addMenus( utilityMenus );
 
         final Menus menus = MenuFactory
-                .newTopLevelMenu( "Authoring" ).perspective( "AuthoringPerspective" )
+                .newTopLevelMenu( AppConstants.INSTANCE.Home() )
+                .respondsWith( new Command() {
+                    @Override
+                    public void execute() {
+                        if ( defaultPerspective != null ) {
+                            placeManager.goTo( new DefaultPlaceRequest( defaultPerspective.getIdentifier() ) );
+                        } else {
+                            Window.alert( "Default perspective not found." );
+                        }
+                    }
+                } )
                 .endMenu()
-                .newTopLevelMenu( "LPR" ).perspective( "LPRPerspective" )
+                .newTopLevelMenu( AppConstants.INSTANCE.Perspectives() )
+                .withItems( getPerspectives() )
                 .endMenu()
                 .newTopLevelMenu( CommonConstants.INSTANCE.ResetPerspectivesTooltip() ).respondsWith( getResetPerspectivesCommand() )
                 .endMenu()
@@ -143,6 +172,18 @@ public class DroolsWorkbenchEntryPoint {
 
         menubar.addMenus( menus );
     }
+
+    private List<Menus> getRoles() {
+        final List<Menus> result = new ArrayList<Menus>( identity.getRoles().size() );
+        result.add( MenuFactory.newSimpleItem( AppConstants.INSTANCE.Logout() ).respondsWith( new LogoutCommand() ).endMenu().build() );
+        for ( final Role role : identity.getRoles() ) {
+            if ( !role.getName().equals( "IS_REMEMBER_ME" ) ) {
+                result.add( MenuFactory.newSimpleItem( AppConstants.INSTANCE.Role() + ": " + role.getName() ).endMenu().build() );
+            }
+        }
+        return result;
+    }
+
 
     private Command getResetPerspectivesCommand() {
         return new Command() {
@@ -167,7 +208,7 @@ public class DroolsWorkbenchEntryPoint {
     }
 
 
-/*
+
     private List<MenuItem> getPerspectives() {
         final List<MenuItem> perspectives = new ArrayList<MenuItem>();
         for ( final PerspectiveActivity perspective : getPerspectiveActivities() ) {
@@ -178,7 +219,6 @@ public class DroolsWorkbenchEntryPoint {
 
         return perspectives;
     }
-*/
 
     private AbstractWorkbenchPerspectiveActivity getDefaultPerspectiveActivity() {
         AbstractWorkbenchPerspectiveActivity defaultPerspective = null;
@@ -198,7 +238,6 @@ public class DroolsWorkbenchEntryPoint {
         return defaultPerspective;
     }
 
-/*
     private List<PerspectiveActivity> getPerspectiveActivities() {
 
         //Get Perspective Providers
@@ -219,7 +258,6 @@ public class DroolsWorkbenchEntryPoint {
 
         return sortedActivities;
     }
-*/
 
     //Fade out the "Loading application" pop-up
     private void hideLoadingPopup() {
@@ -237,6 +275,20 @@ public class DroolsWorkbenchEntryPoint {
                 e.getStyle().setVisibility( Style.Visibility.HIDDEN );
             }
         }.run( 500 );
+    }
+
+    private class LogoutCommand implements Command {
+
+        @Override
+        public void execute() {
+            authService.call( new RemoteCallback<Void>() {
+                @Override
+                public void callback( final Void response ) {
+                    final String location = GWT.getModuleBaseURL().replaceFirst( "/" + GWT.getModuleName() + "/", "/logout.jsp" );
+                    redirect( location );
+                }
+            } ).logout();
+        }
     }
 
     public static native void redirect( String url )/*-{
