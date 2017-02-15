4636862e3e5e5c9e9fc9dd24182c7b5aa6bb65d5
TueCN <tue.tn1@gmail.com>
2017-02-15 18:49:01 +0100

    Implementing isDirty

--- drools-wb-webapp/src/main/java/org/drools/workbench/client/perspectives/LPRPerspective.java
+++ drools-wb-webapp/src/main/java/org/drools/workbench/client/perspectives/LPRPerspective.java
@@ -1,5 +1,8 @@
 package org.drools.workbench.client.perspectives;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -7,12 +10,18 @@ import javax.inject.Inject;
 import org.drools.workbench.client.docks.LPRWorkbenchDocks;
 import org.drools.workbench.client.resources.i18n.AppConstants;
 import org.kie.workbench.common.widgets.client.handlers.lpr.NewRulesMenu;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.uberfire.client.annotations.Perspective;
 import org.uberfire.client.annotations.WorkbenchMenu;
 import org.uberfire.client.annotations.WorkbenchPerspective;
+import org.uberfire.client.mvp.Activity;
+import org.uberfire.client.mvp.ActivityManager;
 import org.uberfire.client.mvp.PlaceManager;
+import org.uberfire.client.mvp.WorkbenchActivity;
 import org.uberfire.client.workbench.panels.impl.MultiListWorkbenchPanelPresenter;
 import org.uberfire.mvp.Command;
+import org.uberfire.mvp.PlaceRequest;
 import org.uberfire.workbench.model.PerspectiveDefinition;
 import org.uberfire.workbench.model.impl.PerspectiveDefinitionImpl;
 import org.uberfire.workbench.model.menu.MenuFactory;
@@ -27,14 +36,19 @@ public class LPRPerspective {
     private NewRulesMenu newRulesMenu;
 
     @Inject
+    private ActivityManager activityManager;
+
+    @Inject
     private PlaceManager placeManager;
 
     @Inject
     private LPRWorkbenchDocks docks;
 
+    private static final Logger log = LoggerFactory.getLogger( LPRPerspective.class );
+
     @PostConstruct
     public void setup() {
-        docks.setup("LPRPerspective" );
+        docks.setup( "LPRPerspective" );
         listenForPostMessage();
     }
 
@@ -43,26 +57,51 @@ public class LPRPerspective {
 //        window.onbeforeunload = function() {return "test";};
 //    }-*/;
 
-    private final native void listenForPostMessage() /*-{
+    private native void listenForPostMessage() /*-{
         console.log('Drools-WB listening for messages');
         var that = this;
-        $wnd.addEventListener("message", function(event) {
-            $wnd.alert('Drools-WB message received: ' + event.data);
+        $wnd.addEventListener("message", function (event) {
+//        $wnd.setTimeout(function () {
             console.log('Drools-WB message received: ', event.data);
-            var dirty = that.@org.drools.workbench.client.perspectives.LPRPerspective::isDirty(Ljava/lang/String;Ljava/lang/String;)(event.data, event.origin);
-            console.log('Drools-WB sending message:', event.data);
-            event.source.postMessage('Hello back - dirty='+dirty, event.origin);
-        });
+//          todo filter out messages not from our drools-wb application
+//          var origin = event.origin || event.originalEvent.origin; // For Chrome, the origin property is in the event.originalEvent object.
+//          if (origin !== "http://example.org:8080")
+//                return;
+            try {
+                if (event.data === 'isDirty') {
+                    var activityManager = that.@org.drools.workbench.client.perspectives.LPRPerspective::activityManager;
+//                    console.log('activityManager', activityManager);
+                    var startedActivities = activityManager.@org.uberfire.client.mvp.ActivityManagerImpl::startedActivities;
+//                    console.log('startedActivities', startedActivities);
+                    var dirty = that.@org.drools.workbench.client.perspectives.LPRPerspective::isDirty(*)(startedActivities);
+                    var message = {message: dirty, isResponseTo: 'isDirty'};
+                    console.log('Drools-WB sending message:', message);
+                    event.source.postMessage(message, event.origin);
+                }
+            } catch (e) {
+                console.error(e);
+            }
+//        }, 10000);
+        }, false);
     }-*/;
 
-    private boolean isDirty( String data, String origin) {
-        return false;
+    private boolean isDirty( Map<Activity, PlaceRequest> startedActivities ) {
+        boolean dirty = false;
+        log.info( "Calling isDirty with startedActivities=" + Arrays.toString( startedActivities.entrySet().toArray() ) );
+        for ( Activity activity : startedActivities.keySet() ) {
+            log.info( "Checking Activity: " + activity.getIdentifier() );
+            if ( !dirty && activity instanceof WorkbenchActivity ) {
+                dirty = !(( WorkbenchActivity ) activity).onMayClose();
+                log.info( "WorkbenchActivity: " + activity.getIdentifier() + " is " + dirty + " dirty" );
+            }
+        }
+        return dirty;
     }
 
     @Perspective
     public PerspectiveDefinition buildPerspective() {
-        final PerspectiveDefinitionImpl perspective = new PerspectiveDefinitionImpl(MultiListWorkbenchPanelPresenter.class.getName());
-        perspective.setName("LPR");
+        final PerspectiveDefinitionImpl perspective = new PerspectiveDefinitionImpl( MultiListWorkbenchPanelPresenter.class.getName() );
+        perspective.setName( "LPR" );
 
         return perspective;
     }
@@ -70,8 +109,8 @@ public class LPRPerspective {
     @WorkbenchMenu
     public Menus buildMenuBar() {
         return MenuFactory
-                .newTopLevelMenu(AppConstants.INSTANCE.New())
-                .withItems( newRulesMenu.getMenuItems())
+                .newTopLevelMenu( AppConstants.INSTANCE.New() )
+                .withItems( newRulesMenu.getMenuItems() )
                 .endMenu()
                 .newTopLevelMenu( AppConstants.INSTANCE.assetSearch() ).position( MenuPosition.RIGHT ).respondsWith( new Command() {
                     @Override
